cf325c8c8fc2ee00a0f4a7fd256db2fc771e93e1
Jakob Frederiksen <jafre13@student.sdu.dk>
2022-10-04 23:38:54 +0200

    Initial commit

---
+++ Core/src/main/java/jaf/components/EvenOddApplication.java
@@ -0,0 +1,26 @@
+package jaf.components;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(scanBasePackages = "jaf.components")
+@RestController
+public class EvenOddApplication {
+
+    private EvenOddService evenOddService;
+
+    // constructor
+
+    @GetMapping("/validate/")
+    public String isEvenOrOdd(
+            @RequestParam("number") Integer number) {
+        return evenOddService.isEvenOrOdd(number);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvenOddApplication.class, args);
+    }
+}
---
+++ src/main/java/jaf/components/Main.java
@@ -0,0 +1,7 @@
+package jaf.components;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
